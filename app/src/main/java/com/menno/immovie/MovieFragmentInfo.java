57b2614b8328c98578e28fadd30a8b4b0a3075c1
Menno Sijben <mennosijben@gmail.com>
2015-11-26 21:16:49 +0100

    fix: Tablet view

--- app/src/main/java/com/menno/immovie/MovieFragmentInfo.java
+++ app/src/main/java/com/menno/immovie/MovieFragmentInfo.java
@@ -1,16 +1,10 @@
 package com.menno.immovie;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.squareup.picasso.Picasso;
 
 /**
  * Created by Menno on 25-11-2015.
@@ -25,30 +19,30 @@ public class MovieFragmentInfo extends Fragment
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_movie_info, container, false);
 
-        Activity activity = getActivity();
-        Intent intent = activity.getIntent();
-        TextView textView = (TextView) rootView.findViewById(R.id.myImageViewText);
+       // Activity activity = getActivity();
+        //Intent intent = activity.getIntent();
+       // TextView textView = (TextView) rootView.findViewById(R.id.myImageViewText);
 
-        Bundle b = intent.getExtras();
-        Movie movie = b.getParcelable(Intent.EXTRA_TEXT);
-        textView.setText(movie.name);
+       // Bundle b = intent.getExtras();
+       // Movie movie = b.getParcelable(Intent.EXTRA_TEXT);
+       // textView.setText(movie.name);
 
-        ImageView iconView = (ImageView) rootView.findViewById(R.id.myImageView);
-        if(iconView != null) {
-            Picasso.with(activity).load("http://image.tmdb.org/t/p/w342" + movie.imageMenuUrl).fit().into(iconView);
+       // ImageView iconView = (ImageView) rootView.findViewById(R.id.myImageView);
+       // if(iconView != null) {
+       //     Picasso.with(activity).load("http://image.tmdb.org/t/p/w342" + movie.imageMenuUrl).fit().into(iconView);
 
-        }
-        ImageView posterView = (ImageView) rootView.findViewById(R.id.posterimage);
-        Picasso.with(activity).load("http://image.tmdb.org/t/p/w185" + movie.imageUrl).fit().into(posterView);
+       // }
+       // ImageView posterView = (ImageView) rootView.findViewById(R.id.posterimage);
+       // Picasso.with(activity).load("http://image.tmdb.org/t/p/w185" + movie.imageUrl).fit().into(posterView);
 
-        TextView overviewText = (TextView) rootView.findViewById(R.id.movie_overview);
-        overviewText.setText(movie.overview);
+      //  TextView overviewText = (TextView) rootView.findViewById(R.id.movie_overview);
+       // overviewText.setText(movie.overview);
 
-        TextView datumText = (TextView) rootView.findViewById(R.id.movie_datum);
-        datumText.setText(movie.releaseDate);
+       // TextView datumText = (TextView) rootView.findViewById(R.id.movie_datum);
+       // datumText.setText(movie.releaseDate);
 
-        TextView scoreText = (TextView) rootView.findViewById(R.id.movie_score);
-        scoreText.setText(movie.rating + "/10");
+       // TextView scoreText = (TextView) rootView.findViewById(R.id.movie_score);
+        //scoreText.setText(movie.rating + "/10");
 
 
         return rootView;
